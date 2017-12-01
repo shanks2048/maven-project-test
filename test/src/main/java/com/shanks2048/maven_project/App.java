eb17d979efddd5af8f7d98e60c70685bdf6b4069
shanks2048 <289271213@qq.com>
2017-12-01 10:25:13 +0800

    test
    test

---
+++ test/src/main/java/com/shanks2048/maven_project/App.java
@@ -0,0 +1,13 @@
+package com.shanks2048.maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
